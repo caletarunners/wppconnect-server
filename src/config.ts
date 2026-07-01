4ae36c2d655ad292bedb11c5b8f8d17401aafd2f
caletarunners <webmaster.caletarunners@gmail.com>
2026-06-30 20:46:45 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { ServerOptions } from './types/ServerOptions';
 
 export default {
-  secretKey: 'THISISMYSECURETOKEN',
+  secretKey: 'MI_SECRETO_DE_RENDER',
   host: 'http://localhost',
   port: '21465',
   deviceName: 'WppConnect',
